fa90fd504486550ac0b95c954308fee0fe993e78
steli <stelian.botnari@gmail.com>
2024-10-19 10:25:55 +0200

    zeus2

--- src/Lesson9/Exmpl2.java
+++ src/Lesson9/Exmpl2.java
@@ -2,6 +2,6 @@ package Lesson9;
 
 public class Exmpl2 {
     public static void main(String[] args) {
-        System.out.println("Hello, second time");
+        System.out.println("Hello, second time tralala");
     }
 }
