d7fea936d32976ffac2a09aa8d0c59ed1ad29d96
AlexandruVarzari <102518596+AlexandruVarzari@users.noreply.github.com>
2024-11-05 12:00:47 +0200

    L13, testing

---
+++ src/Lesson13Tests/Calculations.java
@@ -0,0 +1,12 @@
+package Lesson13Tests;
+
+public class Calculations {
+
+    public int Sum(int a, int b){
+        return a+b;
+    }
+
+    public int Minus(int a, int b){
+        return a-b;
+    }
+}
---
+++ src/Lesson13Tests/MainTest.java
@@ -0,0 +1,59 @@
+package Lesson13Tests;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertTrue;
+
+public class MainTest {
+    @Test
+    public void equality(){
+        String value = "3.13";
+        String value1 = "1";
+        Assert.assertFalse(value1.equals(value));
+    }
+
+    @Test
+    public void testSum(){
+          Calculations sum = new Calculations();
+          int result = sum.Sum(2,3);
+          assertTrue(result==5);
+    }
+
+    @Test
+    public void testMinus(){
+        Calculations minus = new Calculations();
+        int result = minus.Minus(2,2);
+        assertTrue(result==0);
+    }
+
+    @Test
+    public void isPositive(){
+        int num = 3;
+        assertTrue(num > 0);
+    }
+
+    @Test
+   public void arrEqual(){
+        int arrFirst[] = {1,2,3,5};
+        int arrSecond[] = {1,2,3,5};
+        Assert.assertArrayEquals(arrFirst, arrSecond);
+    }
+
+    @Test
+    public void sort(){
+        int arr[]={1,2,4};
+        int sortedArr[] = Arrays.copyOf(arr, arr.length);
+        Arrays.sort(sortedArr);
+        assertArrayEquals(sortedArr, arr);
+    }
+
+    @Test
+    public void isString(){
+        String str = "Hello World";
+        assertTrue(str.contains("Hello"));
+    }
+}
